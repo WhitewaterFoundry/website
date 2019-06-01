7b848ca2bb26566fe52bd6471ea001787f7ac99d
Kevin Brock <kevin.brock@hotmail.com>
2019-06-01 06:32:02 -0700

    split out MenuLink to own file, MenuLink key is now item.id

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
 import MenuItem from "./MenuItem"
+import MenuLink from "./MenuLink"
 
 const MainMenuWrapper = styled.div`
   width: 100%;
@@ -19,12 +19,6 @@ const MenuLinksWrapper = styled.div`
   width: 100%;
   height: 100%;
 `
-
-const MainMenuLink = styled(Link)`
-  text-decoration: none;
-  margin-left: 6px;
-  margin-right: 6px;
-`
 interface Props {
   menuItems: Array<MenuItem>
   renderIcon: Function
@@ -34,11 +28,11 @@ const MainMenu: React.FC<Props> = ({ menuItems, renderIcon }) => {
     <MainMenuWrapper>
       <div style={{ width: "70px" }}>{renderIcon()}</div>
       <MenuLinksWrapper>
-        {menuItems.map((item, i) => {
+        {menuItems.map((item: MenuItem) => {
           return (
-            <MainMenuLink to={item.link} key={`main-menu-item${i}`}>
+            <MenuLink to={item.link} key={item.id}>
               {item.title}
-            </MainMenuLink>
+            </MenuLink>
           )
         })}
       </MenuLinksWrapper>
