68d6a68cf2ebd218c4b330d9670b99d979dfb247
Kevin Brock <kevin.brock@hotmail.com>
2019-06-01 06:41:30 -0700

    Moved  MenuLink to own file

---
+++ src/components/MainMenu/MenuLink.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+const MenuLink = styled(Link)`
+  text-decoration: none;
+  margin-left: 6px;
+  margin-right: 6px;
+`
+
+export default MenuLink
