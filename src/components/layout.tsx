605b54b9817dac3a310d61608693814657f5cce3
Kevin Brock <kevin.brock@hotmail.com>
2019-06-09 08:47:34 -0700

    improved mobile layout and added portrait

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,39 +1,53 @@
-import React, { useState, useContext } from "react"
+import React, { useState } from "react"
+import styled from "styled-components"
 import { useMediaQueries } from "../hooks/useMediaQueries"
 import { MediaContext } from "./MediaContext"
 import Header from "./header"
 import { useMenuItems } from "./MainMenu/useMenuItems"
 import "./layout.css"
 
+const HeaderWrapper = styled.div<{ mobile: boolean }>`
+  ${({ mobile }) => (mobile ? "position: fixed; top: 0;" : "position: absolute; top: 0;")}
+  width: 100%;
+`
+
 const Layout: React.FC = ({ children }) => {
   const [media, setCurrentMedia] = useState("none")
+  const [portrait, setPortrait] = useState("none")
 
   useMediaQueries(
     [
       ["mobile", "(max-width: 480px)"],
       ["small", "(min-width: 480px)"],
       ["medium", "(min-width: 800px)"],
-      ["large", "(min-width: 1200px)"],
+      ["large", "(min-width: 900px)"],
     ],
     setCurrentMedia
   )
 
+  useMediaQueries([["portrait", "(orientation: portrait)"]], setPortrait)
+
   const menuItems = useMenuItems()
+  const mobile = media === "mobile" || media === "small"
 
   return (
     <>
-      <Header menuItems={menuItems} mobile={media === "mobile" || media === "small"} />
       <div
         style={{
           margin: `0 auto`,
           padding: `0`,
-          paddingTop: 0,
+          paddingTop: "72px",
         }}
       >
         <main>
-          <MediaContext.Provider value={{ media: media }}>{children}</MediaContext.Provider>
+          <MediaContext.Provider value={{ media: media, portrait: portrait === "portrait" }}>
+            {children}
+          </MediaContext.Provider>
         </main>
         <footer>© {new Date().getFullYear()}, Whitewater Foundry</footer>
+        <HeaderWrapper mobile={mobile}>
+          <Header menuItems={menuItems} mobile={mobile} />
+        </HeaderWrapper>
       </div>
     </>
   )
