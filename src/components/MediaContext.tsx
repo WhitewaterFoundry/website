dc0340e1e48849eaf565ff2bccf5fb8b7b406996
Kevin Brock <kevin.brock@hotmail.com>
2019-06-09 08:32:44 -0700

    Added portrait state

--- src/components/MediaContext.tsx
+++ src/components/MediaContext.tsx
@@ -1,2 +1,5 @@
 import React from "react"
-export const MediaContext = React.createContext<{ media: string | null }>({ media: null })
+export const MediaContext = React.createContext<{ media: string | null; portrait: boolean }>({
+  media: null,
+  portrait: false,
+})
