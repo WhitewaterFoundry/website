4d96e07cd8b24b2bf5150fb7ee171fc190beff81
Kevin Brock <kevin.brock@hotmail.com>
2019-07-03 09:37:49 -0700

    Created contact form

---
+++ src/components/ContactForm/ContactForm.test.tsx
@@ -0,0 +1,56 @@
+import * as React from "react"
+import { render, cleanup } from "react-testing-library"
+import "jest-dom/extend-expect"
+import ContactForm from "./ContactForm"
+import "react-testing-library/cleanup-after-each"
+
+afterEach(() => {
+  cleanup()
+  jest.clearAllMocks()
+})
+
+const renderContactForm = () => render(<ContactForm headingText="Test Heading Text" />)
+
+describe("<EnterpriseLinuxDistros />", () => {
+  it("Has a Name label with an input type=text as it's child", () => {
+    const { getByLabelText } = renderContactForm()
+    const nameField = getByLabelText("Name")
+    expect(nameField.getAttribute("type")).toBe("text")
+  })
+
+  it("Has a Title label with an input type=text as it's child", () => {
+    const { getByLabelText } = renderContactForm()
+    const titleField = getByLabelText("Title")
+    expect(titleField.getAttribute("type")).toBe("text")
+  })
+
+  it("Has an Organization label with an input type=text as it's child", () => {
+    const { getByLabelText } = renderContactForm()
+    const orgField = getByLabelText("Organization")
+    expect(orgField.getAttribute("type")).toBe("text")
+  })
+
+  it("Has an Email label with an input type=email as it's child", () => {
+    const { getByLabelText } = renderContactForm()
+    const emailField = getByLabelText("Email")
+    expect(emailField.getAttribute("type")).toBe("email")
+  })
+
+  it("Has an 'Estimated Number of Seats' label with an input type=number as it's child", () => {
+    const { getByLabelText } = renderContactForm()
+    const seatsField = getByLabelText("Estimated Number of Seats")
+    expect(seatsField.getAttribute("type")).toBe("number")
+  })
+
+  it("Has a 'Phone Number' label with an input type=tel as it's child", () => {
+    const { getByLabelText } = renderContactForm()
+    const phoneField = getByLabelText("Phone Number")
+    expect(phoneField.getAttribute("type")).toBe("tel")
+  })
+
+  it("Has a submit button", () => {
+    const { getByText } = renderContactForm()
+    const submitButton = getByText("Submit")
+    expect(submitButton.getAttribute("type")).toBe("submit")
+  })
+})
---
+++ src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,100 @@
+import React, { FormEvent, forwardRef, CSSProperties } from "react"
+import styled from "styled-components"
+
+const Input = styled.input`
+  border: none;
+  border-radius: 3px;
+  background-color: #f8e8ff;
+  margin-left: 8px;
+  height: 2em;
+`
+
+const TextArea = styled.textarea`
+  border: none;
+  border-radius: 3px;
+  background-color: #f8e8ff;
+  margin-left: 8px;
+  margin-top: 0;
+`
+
+const InputWrapper = styled.div`
+  margin-top: 0.8em;
+  width: 100%;
+  display: grid;
+  grid-template-rows: auto;
+`
+
+const SubmitButton = styled.button`
+  border: none;
+  border-radius: 4px;
+  background: linear-gradient(256.8deg, #9755b7 -8.63%, #5c2d72 105.85%);
+  margin-top: 1em;
+  margin-left: 8px;
+  font-size: 1.2em;
+  color: #fefbff;
+  padding: 8px 24px 8px 24px;
+`
+
+interface ContactFormProps {
+  headingText: string
+  action?: string
+  onSubmit?: (event: FormEvent) => void
+  containerId?: string
+  style?: CSSProperties
+}
+
+const ContactForm = forwardRef<HTMLDivElement, ContactFormProps>(
+  ({ headingText, action, onSubmit, containerId, style }, ref) => {
+    return (
+      <div
+        id={containerId}
+        ref={ref}
+        style={{
+          padding: ".5em 1em 1em 1em",
+          background: "linear-gradient(103.37deg, #212121 4.29%, rgba(33, 33, 33, 0.89) 107.97%)",
+          color: "#fefbff",
+          maxWidth: "900px",
+          borderRadius: "4px",
+          ...style,
+        }}
+      >
+        <h1 style={{ textAlign: "center", marginTop: "1em" }}>{headingText}</h1>
+        <form action={action} onSubmit={onSubmit} method="POST" style={{ margin: "1em" }}>
+          <InputWrapper>
+            <label htmlFor="name">Name</label>
+            <Input type="text" id="name" required={true} />
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="email">Email</label>
+            <Input type="email" id="email" required={true} />
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="title">Title</label>
+            <Input type="text" id="title" required={true} />
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="organization">Organization</label>
+            <Input type="text" id="organization" required={true} />
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="phone">Phone Number</label>
+            <Input type="tel" id="phone" />
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="numSeats">Estimated Number of Seats</label>
+            <Input type="number" id="numSeats" />
+          </InputWrapper>
+          <InputWrapper>
+            <label htmlFor="interestedIn">I am interested in...</label>
+            <TextArea id="interestedIn" rows={3} />
+          </InputWrapper>
+          <div style={{ display: "flex", justifyContent: "center", marginTop: "1.5em" }}>
+            <SubmitButton type="submit">Submit</SubmitButton>
+          </div>
+        </form>
+      </div>
+    )
+  }
+)
+
+export default ContactForm
