d1e7a81f65502918b19d5d33e750542f585e1f3d
Kevin Brock <kevin.brock@hotmail.com>
2019-07-05 06:17:33 -0700

    Improved icon animation and browser compatibility

--- src/components/MainMenu/MenuIcon.tsx
+++ src/components/MainMenu/MenuIcon.tsx
@@ -1,13 +1,45 @@
 import React from "react"
 import styled from "styled-components"
+import posed from "react-pose"
 
-const Rect: React.FC<{
-  className: string
+interface PosedRectProps {
+  closeWidth: number
+  closeX: number
+  closeY: number
+  closeRotate: number
+  hamburgerWidth: number
   x: string
   y: string
-  width: string
-  height: string
-}> = props => <rect {...props} x="4" height="2" fill="#363636" rx="1" />
+  width?: string
+  height?: string
+  pose: "menu-visible" | "menu-hidden"
+  className?: string
+}
+
+const PosedRect = posed.rect<{ closeWidth: number }>({
+  "menu-visible": {
+    width: (props: PosedRectProps) => props.closeWidth,
+    x: (props: PosedRectProps) => props.closeX,
+    y: (props: PosedRectProps) => props.closeY,
+    rotate: (props: PosedRectProps) => props.closeRotate,
+    transition: {
+      default: { ease: "easeIn", duration: 120 },
+    },
+  },
+  "menu-hidden": {
+    width: (props: PosedRectProps) => props.hamburgerWidth,
+    x: 0,
+    y: 0,
+    rotate: 0,
+    transition: {
+      default: { ease: "easeIn", duration: 120 },
+    },
+  },
+})
+
+const Rect: React.FC<PosedRectProps> = props => (
+  <PosedRect {...props} x="4" height="2" fill="#363636" rx="1" />
+)
 
 const ButtonWrapper = styled.button`
   background: none;
@@ -28,55 +60,54 @@ const IconWrapper = styled.div`
     transition: all 150ms ease-in;
     transform-origin: 50% 50%;
   }
-
-  .line-end {
-    width: 38px;
-  }
-
-  .line-x {
-    transform: translateX(-14px) translateY(2px) rotate(45deg);
-    width: 51px;
-  }
-
-  .line-y {
-    transform: translateX(-18px) translateY(-2px) rotate(-45deg);
-    width: 51px;
-  }
-
-  .line-z {
-    width: 0;
-    transform: translateX(20px);
-  }
 `
 
-const MenuIcon: React.FC<{ menuVisible: boolean; clickHandler: any }> = ({
-  menuVisible,
-  clickHandler,
-}) => {
+const MenuIcon: React.FC<{
+  menuVisible: boolean
+  clickHandler: ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void) | undefined
+  scale: number
+}> = ({ menuVisible, clickHandler, scale = 1 }) => {
+  const linePose = menuVisible ? "menu-visible" : "menu-hidden"
   return (
     <ButtonWrapper onClick={clickHandler}>
       <IconWrapper>
-        <svg id="icon" width="40" height="40">
+        <svg id="icon" width={String(40 * scale)} height={String(40 * scale)} viewBox="0 0 40 40">
           <Rect
-            className={`line ${menuVisible ? "line-x" : ""}`}
+            closeWidth={51}
+            hamburgerWidth={34}
+            className={"line"}
+            closeX={-14}
+            closeY={2}
+            closeRotate={45}
             x="4"
             y="10"
-            width="34"
             height="2"
+            pose={linePose}
           />
           <Rect
-            className={`line ${menuVisible ? "line-z" : ""}`}
+            closeWidth={0}
+            hamburgerWidth={28}
+            className={"line"}
+            closeX={20}
+            closeY={0}
+            closeRotate={0}
             x="4"
             y="21"
             width="28"
             height="2"
+            pose={linePose}
           />
           <Rect
-            className={`line ${menuVisible ? "line-y" : ""}`}
+            closeWidth={51}
+            hamburgerWidth={34}
+            className={"line"}
+            closeX={-18}
+            closeY={-2}
+            closeRotate={-45}
             x="4"
             y="34"
-            width="34"
             height="2"
+            pose={linePose}
           />
         </svg>
       </IconWrapper>
