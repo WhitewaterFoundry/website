3c19098349c49255ef312e8040832be8090fc9e8
Kevin Brock <kevin.brock@hotmail.com>
2019-08-08 12:34:56 -0700

    Throw error on duplicate paths in blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,7 @@ const path = require("path")
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
+
   const blogPostTemplate = path.resolve(`src/components/Blog/BlogTemplate.js`)
 
   return graphql(`
@@ -26,7 +27,18 @@ exports.createPages = ({ graphql, actions }) => {
       throw result.errors
     }
 
+    let uniquePathVerification = {}
+
     result.data.allMdx.edges.forEach(edge => {
+      const path = edge.node.frontmatter.path
+      if (uniquePathVerification[path] === true) {
+        throw Error(
+          `An error occured while creating blog posts: A blog post with the path ${path} already exists`
+        )
+      }
+
+      uniquePathVerification[path] = true
+
       createPage({
         path: edge.node.frontmatter.path,
         component: blogPostTemplate,
