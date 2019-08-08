1c489c59cb77c505e25ccd3d91eceb4a96a545ae
Kevin Brock <kevin.brock@hotmail.com>
2019-08-08 11:20:16 -0700

    decreased link size and margin

--- src/components/MainMenu/MenuLink.tsx
+++ src/components/MainMenu/MenuLink.tsx
@@ -3,8 +3,10 @@ import { Link } from "gatsby"
 
 const MenuLink = styled(Link)`
   text-decoration: none;
-  margin-left: 6px;
-  margin-right: 6px;
+  text-align: center;
+  font-size: 0.9em;
+  margin-left: 2px;
+  margin-right: 2px;
 `
 
 export default MenuLink
